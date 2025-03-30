bb14bae03bff7f194b4914957837bd7ccbafd940
Satsawat <satsawatrodketkul@gmail.com>
2025-03-30 22:41:47 +0700

    Init project

---
+++ internal/database/database_connection.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"user-profile-service/internal/config"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func DBconnection() {
+	dbHost := config.GetValue("DATABASE_HOST")
+	dbPort := config.GetValue("DATABASE_PORT")
+	dbUser := config.GetValue("DATABASE_USER")
+	dbPassword := config.GetValue("DATABASE_PASSWORD")
+	dbName := config.GetValue("DATABASE_NAME")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	db, err := gorm.Open(postgres.Open(dsn))
+
+	if err != nil {
+		log.Fatal("Failed to connect database:", err)
+	}
+
+	log.Print("Connect to database success:", db)
+}
